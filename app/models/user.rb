e49d7d710ae8ba922123b533cf445863c091d5e1
花籠一樹 <graphic.ocean0906@gmail.com>
2017-11-12 14:16:49 +0900

    マイページ機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  def name
+    "#{family_name} #{first_name}"
+  end
+
+  def name_kana
+    "#{family_name_kana} #{first_name_kana}"
+  end
+
+  private
+  def has_group_key?
+    group_key.present?
+  end
+
+  def group_key_to_id
+    group = Group.where(key: group_key).first_or_create
+    self.group_id = group.id
+  end
 end
