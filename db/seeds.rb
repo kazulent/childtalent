74f6f98e223f8de64c9949341ba61eb595c10ac2
花籠一樹 <graphic.ocean0906@gmail.com>
2017-12-06 13:17:29 +0900

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: 'graphic.oean0906@gmail.com', password: 'mirei270906', nickname: 'Kazuki')
